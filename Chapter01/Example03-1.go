9b4445cc347acc2eb9d06dcee6d4147b15e1fec2
kimmayer <lkimilhol@gmail.com>
2019-01-26 17:46:06 +0900

    fix a Example03-1's comment write a Example03-2

--- Chapter01/Example03-1.go
+++ Chapter01/Example03-1.go
@@ -12,7 +12,7 @@ func main() {
 	for input.Scan() {
 		counts[input.Text()]++
 	}
-	// NOTE: input.Err()에서의 잠재적 오류는 뮈한다.
+	// NOTE: input.Err()에서의 잠재적 오류는 무시한다.
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
